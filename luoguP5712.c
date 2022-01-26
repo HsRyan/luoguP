34fdafce535b6d609d57a38cc2075f72e1563e91
Ryan <2112313118@qq.com>
2022-01-26 11:25:41 +0800

    Create luoguP5712.c

---
+++ luoguP5712.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if (a > 1)
+        printf("Today,I ate %d apples.\n", a);
+    else
+        printf("Today,I ate %d apple.\n", a);
+    return 0;
+}
